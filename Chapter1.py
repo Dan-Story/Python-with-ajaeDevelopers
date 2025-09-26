a9aaf08a04443b44c82c0b84801ac749ce4aa0c6
Dan-Story <mhoo0421@gmail.com>
2025-09-26 19:32:22 +0900

    Test : VS Code 연동 확인 - 1

---
+++ Chapter1.py
@@ -0,0 +1,19 @@
+# print("Hello World")
+
+# input("입력:")
+# text = input("문자를 입력하세요: ")
+# print("'" + text + "'")
+
+# x = 10
+# y = 20
+# x, y = y, x
+# print("x:", x)
+# print("y:", y)
+
+# text = input("입력:")
+# print("'" + text + "'")
+
+# x = int(input("입력1:"))
+# y = int(input("입력2:"))
+# print(x)
+# print(y)
---
+++ Chapter2.py
@@ -0,0 +1,27 @@
+# a, b, c = 10, -5, 0
+# print(a, b, c)
+
+# a, b, c = 0b101, 0o123, 0xABC
+# print(a, b, c)
+
+# a = int(input("입력1: "))
+# b = int(input("입력2: "))
+# print(a*b)
+
+# a = int(input("입력1: "))
+# b = int(input("입력2: "))
+# print(a>5 and b>5)
+
+# a = int(input("입력1: "))
+# b = int(input("입력2: "))
+# print("몫:", a//b)
+# print("나머지:", a%b)
+
+# a, b = 123, 3.14
+# print("{:2}, {:.1f}".format(a, b))
+# print("%d, %.1f" % (a,b))
+
+# a = int(input("입력1: "))
+# b = int(input("입력2: "))
+# c = int(input("입력3: "))
+# print(a>=0 or b>=0 or c>=0)
