bc2649184db6c64eabe138d103f6df76424e15dd
Dan-Story <mhoo0421@gmail.com>
2025-10-10 02:03:14 +0900

    Feat: 3번까지 완료

---
+++ Chapter5.py
@@ -0,0 +1,33 @@
+#1
+# x = int(input("첫 번째 숫자: "))
+# y = int(input("두 번째 숫자: "))
+# if x > y:
+#     print("첫 번쨰 숫자가 더 큼")
+# elif x < y:
+#     print("두 번째 숫자가 더 큼")
+# else:
+#     print("두 숫자는 같음")
+
+#2
+# score = int(input("점수: "))
+# if score >= 90:
+#     grade = "A"
+# elif score >= 80:
+#     grade = "B"
+# elif score >= 70:
+#     grade = "C"
+# elif score >= 60:
+#     grade = "D"
+# else:
+#     grade = "F"
+
+# print("학점:", grade)
+
+#3
+# x = int(input("숫자: "))
+# if x % 2 == 0:
+#     print("짝수")
+# else:
+#     print("홀수")
+
+#4
